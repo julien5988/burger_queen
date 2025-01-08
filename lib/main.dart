17b479cfda1ca7901b3eea9fa4f5b63c37a4c153
Julien <julien.thoumy@gmail.com>
2025-01-08 23:19:17 +0100

    Section accompagnement reste un peut de design a revoir

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   final String title;
   const MyHomePage({super.key, required this.title});
-
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -110,8 +110,8 @@ class MyHomePage extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Card(
-                          elevation: 6,
-                          child: ClipRRect(
+                        elevation: 6,
+                        child: ClipRRect(
                             borderRadius: BorderRadius.circular(16),
                             child: SizedBox(
                                 width: MediaQuery.of(context).size.width * 0.7,
@@ -135,15 +135,15 @@ class MyHomePage extends StatelessWidget {
                                       ),
                                     )
                                   ],
-                                )),
-                          ))
+                                ))),
+                      )
                     ],
-                  )
+                  ),
                 ],
               ),
             ),
             SizedBox(height: 8),
-            Text(
+            const Text(
               "Chaud devant",
               style: TextStyle(
                   color: Colors.brown,
@@ -155,14 +155,47 @@ class MyHomePage extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               child: Row(
                 children: [
-              customCard(image: "assets/images/twins.jpg", title: "Twins", subTitle: "Le burger des jumeaux qui font la paire" , context: context),
-              customCard(image: "assets/images/big-queen.jpg", title: "Big Queen", subTitle: "Pour celles qui portent la couronne à la maison", context: context),
-              customCard(image: "assets/images/egg-bacon-burger.jpg", title: "Egg Bacon", subTitle: "Le burger des lève tôt" , context: context),
-              customCard(image: "assets/images/prince.jpg", title: "Prince", subTitle: "Le préféré des futurs rois", context: context),
-              customCard(image: "assets/images/cheese.jpg", title: "Cheese ", subTitle: " Le classique pour les fans de fromage", context: context)
-              ],
+                  customCard(
+                      image: "assets/images/twins.jpg",
+                      title: "Twins",
+                      subTitle: "Le burger des jumeaux qui font la paire",
+                      context: context),
+                  customCard(
+                      image: "assets/images/big-queen.jpg",
+                      title: "Big Queen",
+                      subTitle:
+                          "Pour celles qui portent la couronne à la maison",
+                      context: context),
+                  customCard(
+                      image: "assets/images/egg-bacon-burger.jpg",
+                      title: "Egg Bacon",
+                      subTitle: "Le burger des lève tôt",
+                      context: context),
+                  customCard(
+                      image: "assets/images/prince.jpg",
+                      title: "Prince",
+                      subTitle: "Le préféré des futurs rois",
+                      context: context),
+                  customCard(
+                      image: "assets/images/cheese.jpg",
+                      title: "Cheese",
+                      subTitle: "Le classique pour les fans de fromage",
+                      context: context)
+                ],
               ),
-            )
+            ),
+            SizedBox(height: 8),
+            const Text(
+              "Les accompagnements",
+              style: TextStyle(
+                  color: Colors.brown,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16),
+            ),
+            rowAccompagnement(image: "assets/images/fries.jpg", title: "Frites"),
+            rowAccompagnement(image: "assets/images/patate-douce.jpg", title: "Frite de patate douce"),
+            rowAccompagnement(image: "assets/images/potatoes.jpg", title: "Potatoes"),
+            rowAccompagnement(image: "assets/images/poutine.jpg", title: "Poutines")
           ],
         ),
       ),
@@ -175,65 +208,106 @@ class MyHomePage extends StatelessWidget {
     required String subTitle,
     required BuildContext context,
   }) {
-    return Padding(padding: EdgeInsets.all(10),
-      child:
-      Card(
-      elevation: 6,
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(16),
-        child: Stack(
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Card(
+        elevation: 6,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(16),
+          child: Stack(
+            children: [
+              // Background Image
+              Image.asset(
+                image,
+                height: 200,
+                width: 250,
+                fit: BoxFit.cover,
+              ),
+              // Overlay color
+              Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  height: 50,
+                  color: Color(0xFFFFC0D3),
+                ),
+              ),
+              // Title
+              Positioned(
+                bottom: 25,
+                left: 0,
+                right: 0,
+                child: Text(
+                  title,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.brown,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              // Subtitle
+              Positioned(
+                bottom: 5,
+                left: 0,
+                right: 0,
+                child: Text(
+                  subTitle,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.brown,
+                    fontSize: 11,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget rowAccompagnement({
+    required String image,
+    required String title,
+    double avatarRadius = 34, // Rayon de l'avatar (valeur par défaut)
+    EdgeInsetsGeometry padding = const EdgeInsets.all(8), // Padding autour de l'élément
+  }) {
+    return Container(
+      color: Color(0xFFFFC0D3), // Couleur de fond
+      child: Padding(
+        padding: padding,
+        child: Column(
           children: [
-            // Image de fond
-            Image.asset(
-              image,
-              height: 200,
-              width: 250,
-              fit: BoxFit.cover,
-            ),
-            // Couleur superposée
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                height: 50, // Hauteur de la superposition
-                color: Color(0xFFFFC0D3), // Couleur avec opacité
-              ),
-            ),
-            // Texte principal
-            Positioned(
-              bottom: 25, // Positionnement pour laisser de l'espace au sous-titre
-              left: 0,
-              right: 0,
-              child: Text(
-                title,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Colors.brown,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                CircleAvatar(
+                  radius: avatarRadius, // Image de l'accompagnement
+                  backgroundImage: AssetImage(image),
                 ),
-              ),
-            ),
-            // Sous-titre
-            Positioned(
-              bottom: 5,
-              left: 0,
-              right: 0,
-              child: Text(
-                subTitle,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Colors.brown,
-                  fontSize: 11,
+                Text(
+                  title,
+                  style: const TextStyle(
+                    color: Colors.red,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
                 ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 3),
+              child: Divider(
+                height: 3,
+                color: Colors.grey,
               ),
             ),
           ],
         ),
       ),
-    ),
     );
   }
 }
-
